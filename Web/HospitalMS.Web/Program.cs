1dd2910f19deb688da96b81c6fa03ee31da1b92f
TsonkoStoyanov <32267325+TsonkoStoyanov@users.noreply.github.com>
2019-11-09 14:56:40 +0200

    added Application Insights

--- Web/HospitalMS.Web/Program.cs
+++ Web/HospitalMS.Web/Program.cs
@@ -13,6 +13,7 @@
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
+                .UseApplicationInsights()
                 .UseStartup<Startup>();
     }
 }
